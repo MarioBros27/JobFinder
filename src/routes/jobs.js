08039ec13e8508b1d9ac8204ba355d739089c596
MarioBros27 <a01187827@itesm.mx>
2022-05-06 10:44:17 -0600

    finished mvp for jobs view

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,8 +1,7 @@
-import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { withRouter } from "react-router";
-
-
+import { useNavigate , Link} from "react-router-dom";
+import { Grid, Typography, Paper } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Button from '@mui/material/Button';
 import React from "react";
 const jobso = [
@@ -21,24 +20,72 @@ const jobso = [
 ]
 
 function Jobs() {
-    let navigate = useNavigate();
+    // let navigate = useNavigate();
 
     return (
         <>
-            {jobso.map(job => {
+            <div style={{ marginTop: 20, marginLeft: 20, }}>
 
 
-                return (
-                    <div key={job.id} style={{marginTop: 20, marginBottom: 20}}>
-                        <Button
+                <Typography variant="h2" component="div" style={{ marginLeft: 20 }}>
+                    Trabajos disponibles para ti:
+                </Typography>
+                
 
-                            onClick={() => { navigate(`/jobs/${job.id}`) }}
-                            
-                            variant="outlined" color="inherit" sx={{ borderRadius: 28, marginLeft: 10 }}> hello world {job.id}</Button>
-                    </div>
-                )
-            })}
-            {/* < Outlet /> */}
+                
+                {jobso.map(job => {
+
+
+                    return (
+                            <Paper
+                                key={job.id} 
+                                elevation={0}
+                                 sx={{ borderRadius: 2, maxWidth: "95%", textTransform: 'none', marginTop: 2, 
+                                 marginBottom: 2,
+                                 padding: 2,
+                                 borderWidth: 1,
+                                 borderColor: "#000",
+                                 borderStyle: "solid"
+                                 }}>
+                                <Grid container alignItems="center" direction="row" columnSpacing={2} >
+                                    <Grid item xs={1}>
+                                        <img style={{ maxWidth: "100%", align: "center" }} src={`${process.env.PUBLIC_URL}/building.png`} />
+
+
+                                    </Grid>
+                                    <Grid item container rowSpacing={0}
+                                        justifyContent="flex-start"
+                                        alignItems="flex-start"
+                                        xs={10}>
+                                        <Grid item xs={12} >
+                                            <Typography component={Link} to={`/jobs/${job.id}`} align="left" variant="h4" sx={{color:"#000",textDecoration: "none"}}>
+                                                Front end developer
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12} align="left">
+                                            <Typography variant="h6" component="div">
+                                                Google
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12} align="left">
+                                            <Typography variant="h8" component="div">
+                                                Chihuahua, Chihuahua, México
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={1} style={{align:"center", justifyContent:"center"}}>
+                                        <IconButton aria-label="bookmark"  sx={{color: "#000"}} >
+                                            <BookmarkIcon />
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+
+                            </Paper>
+                    )
+                    
+                })}
+                {/* < Outlet /> */}
+            </div>
         </>
 
     )
